12629e8eb7aba3ad052b24814b4abd9033929725
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-04 21:52:49 +0700

    Operator Aritmatika - Penugasan - Perbandingan

---
+++ Front End Development/Dasar Pemograman dengan JavaScript/operator-pada-tipe-data-number-javascript/js/script.js
@@ -0,0 +1,25 @@
+// Operator Aritmatika
+// +, -, *, /, %
+console.info(10 + 5);
+console.info(10 - 5);
+console.info(10 * 5);
+console.info(10 / 5);
+console.info(10 % 5);
+console.info(10 % 3);
+console.info(1 + 2 - (3 * 4) / 5);
+
+// Operator Penugasan (Assigment)
+let x = 10;
+console.log(x);
+x = 200;
+console.log(x);
+x += 5;
+console.log(x);
+
+// Operator Perbandingan
+console.log(10 == 10);
+console.log(10 == 15);
+console.log(10 != 10);
+console.log(10 != 15);
+console.log(10 == `10`);
+console.log(10 === `10`);
