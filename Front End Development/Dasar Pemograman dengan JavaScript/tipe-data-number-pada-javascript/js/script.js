89cd4e359087c8ce9519aedc9a1aa8f9a22d167b
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-04 21:08:19 +0700

    Tipe data number pada javascript

---
+++ Front End Development/Dasar Pemograman dengan JavaScript/tipe-data-number-pada-javascript/js/script.js
@@ -0,0 +1,25 @@
+// Tipe data number tanpa desimal
+console.log(10);
+console.log(999999999999999);
+console.log(9999999999999999);
+// Tipe data number dengan desimal
+console.log(3.14);
+console.log(400.12344);
+console.log(0.3 + 0.7);
+console.log(0.2 + 0.1);
+// Tipe data number eksponen
+console.info(123e5);
+console.info(123e-5);
+// Tipe data number negatif
+console.info(-123e-5);
+// Tipe data number oktal
+console.info(022);
+// Tipe data number Heksadesimal
+console.info(0x22);
+console.info(0xff);
+// Tipe data number Special
+console.info(2 / 0);
+console.info(-2 / 0);
+console.info(0 / 0);
+console.info(0 / `0`);
+console.log(100 / `10`);
