ee89d0bbcf3678ce56a8c9fd016b67d5e3ba66b1
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-05 04:09:36 +0700

    Materi Akhir Operator Pada String JavaScript

---
+++ Front End Development/Dasar Pemograman dengan JavaScript/operator-pada-tipe-data-string-javascript/js/script.js
@@ -0,0 +1,19 @@
+let a = `Apple`;
+let b = `Pen`;
+console.info(a + b);
+console.info(a + ` ` + b);
+
+// Penjumlahan Number dengan String
+console.info(10 + `10`);
+console.info(`10` + 10);
+console.info(10 + 10 + `10`);
+console.info(`10` + 10 + 10);
+
+// Operator Typeof
+console.info(typeof 10);
+console.info(typeof `10`);
+console.info(typeof true);
+
+// Operator Kondisional
+let x = 10;
+console.info(x % 2 === 0 ? `genap` : `ganjil`);
