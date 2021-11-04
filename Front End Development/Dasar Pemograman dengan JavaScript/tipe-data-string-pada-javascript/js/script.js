2b29b6377efb9e524521aa07e607de9f7ba37210
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-05 04:24:38 +0700

    Materi String

---
+++ Front End Development/Dasar Pemograman dengan JavaScript/tipe-data-string-pada-javascript/js/script.js
@@ -0,0 +1,16 @@
+console.info(`Hari ini adalah hari Jum'at`);
+console.info(`Kami sedang mengadakan kegiatan "Gerakan Memungut Sampah"`);
+
+// escape character
+console.info("Ini adalah hari Jum`at");
+console.info("Ini adalah hari\n Jum`at");
+console.info("Ini adalah hari\n\n Jum`at");
+console.info(`\u00A9`);
+console.info(`\u2122`);
+
+// Operator Konkatenasi
+
+// Membandinkan 2 buah string
+console.info(`Usep Syaikal Arifin` === `usep syaikal arifin`);
+// Fungsi length pada string
+console.info(`Usep Syaikal Arifin`.length);
