4e9c302234839fc46eacd453fc7322c61fb597aa
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-05 07:32:20 +0700

    Tabel Penelusuran

---
+++ Front End Development/Dasar Pemograman dengan JavaScript/tabel-penelusuran-loop/js/script.js
@@ -0,0 +1,6 @@
+let nomorAngkot = 1;
+let jumlahAngkot = 10;
+while (nomorAngkot <= jumlahAngkot) {
+    document.writeln(`<p>Angkot No . ${nomorAngkot} beroperasi dengan baik</p>`);
+    nomorAngkot++;
+}
