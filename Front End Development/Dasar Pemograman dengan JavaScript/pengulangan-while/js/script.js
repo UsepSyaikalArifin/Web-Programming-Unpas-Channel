c142eeb3217bb5cf17e366c46bc11bac97d6d107
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-05 07:21:36 +0700

    Latihan While Loop

--- Front End Development/Dasar Pemograman dengan JavaScript/pengulangan-while/js/script.js
+++ Front End Development/Dasar Pemograman dengan JavaScript/pengulangan-while/js/script.js
@@ -1,14 +1,28 @@
 // 1. Menghentikan loop oleh user
-let i = true;
-while (i) {
-    document.writeln(`<p>Hello World</p>`);
-    i = confirm(`Lanjut ?`);
-}
+// let i = true;
+// while (i) {
+//     document.writeln(`<p>Hello World</p>`);
+//     i = confirm(`Lanjut ?`);
+// }
 
-// 2. Menghentikan loop oleh program
-let nilaiAwal = 1;
-while (nilaiAwal <= 5) {
-    document.writeln(`<p>Hello World</p>`);
-    document.writeln(`<hr>`);
-    nilaiAwal++;
+// // 2. Menghentikan loop oleh program
+// let nilaiAwal = 1;
+// while (nilaiAwal <= 5) {
+//     document.writeln(`<p>Hello World</p>`);
+//     document.writeln(`<hr>`);
+//     nilaiAwal++;
+// }
+
+// let nilaiAwal = 1;
+// while (nilaiAwal <= 10) {
+//     document.writeln(`<p>Hello World ke : ${nilaiAwal}x`);
+//     document.writeln(`<hr>`);
+//     nilaiAwal++;
+// }
+
+// Latihan
+let i = 1;
+while (i <= 10) {
+    document.writeln(`<p>Angkot No. ${i} beroprasi dengan baik.`);
+    i++;
 }
