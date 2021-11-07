bd7acde69d6dfd98b8a8e5b6c523e4ea91a99b52
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-07 17:18:31 +0700

    Lanjutkan Latihan Pengkondisian JavaScript

--- Front End Development/Dasar Pemograman dengan JavaScript/latihan-pengkondisian-javascript/js/script.js
+++ Front End Development/Dasar Pemograman dengan JavaScript/latihan-pengkondisian-javascript/js/script.js
@@ -2,7 +2,7 @@ let noAngkot = 1
 let jumlahAngkot = 10
 
 for(noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++){
-    if(noAngkot<= 4 && noAngkot === 6){
+    if(noAngkot<= 6 && noAngkot !== 5){
         document.writeln(`<p>Angkot No. ${noAngkot} beroperasi dengan Baik</p>`)
     }else if(noAngkot === 8 || noAngkot === 10 || noAngkot === 5){
         document.writeln(`<p>Angkot No. ${noAngkot} sedang lembur<p>`)
