13d15c14678abf0ff5b0d0c42823ab3676bdd31f
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-07 17:36:20 +0700

    Contoh Latihan Pengkondisian Switch

--- Front End Development/Dasar Pemograman dengan JavaScript/pengkondisian-switch/js/script.js
+++ Front End Development/Dasar Pemograman dengan JavaScript/pengkondisian-switch/js/script.js
@@ -1,4 +1,4 @@
-let angka = parseInt(prompt(`Masukan angka !!!`))
+// let angka = parseInt(prompt(`Masukan angka !!!`))
 
 // if(angka === 1){
 //     document.writeln(`<p>Anda memasukan angka = ${angka}</p>`)
@@ -8,20 +8,43 @@ let angka = parseInt(prompt(`Masukan angka !!!`))
 //     document.writeln(`<p>Anda memasukan angka bukan 1 & 2</p>`)
 // }
 
-switch(angka){
-    case 1:
-        document.writeln(`<p>Anda memasukan angka ${angka}`)
+// switch(angka){
+//     case 1:
+//         document.writeln(`<p>Anda memasukan angka ${angka}`)
+//         break
+//     case 2:
+//         document.writeln(`<p>Anda memasukan angka ${angka}`)
+//         break
+//     case 3:
+//         document.writeln(`<p>Anda memasukan angka ${angka}`)
+//         break
+//     case 4:
+//         document.writeln(`<p>Anda memasukan angka ${angka}`)
+//         break
+//     default:
+//         document.writeln(`<p>Anda memasukan bukan angka valid yaitu ${angka}`)
+//         break
+// }
+
+let item = prompt(`Masukan nama makanan / minuman !!!`)
+
+switch(item){
+    case `nasi`:
+        document.writeln(`<p>nasi adalah makanan / minuman sehat</p>`)
         break
-    case 2:
-        document.writeln(`<p>Anda memasukan angka ${angka}`)
+    case `daging`:
+        document.writeln(`<p>daging adalah makanan / minuman sehat</p>`)
         break
-    case 3:
-        document.writeln(`<p>Anda memasukan angka ${angka}`)
+    case `susu`:
+        document.writeln(`<p>susu adalah makanan / minuman sehat</p>`)
         break
-    case 4:
-        document.writeln(`<p>Anda memasukan angka ${angka}`)
+    case `hamburger`:
+        document.writeln(`<p>hamburger adalah makanan / minuman tidak sehat</p>`)
+        break
+    case `softdrink`:
+        document.writeln(`<p>softdrink adalah makanan / minuman tidak sehat</p>`)
         break
     default:
-        document.writeln(`<p>Anda memasukan bukan angka valid yaitu ${angka}`)
-        break
+        document.writeln(`<p>softdrink adalah makanan / minuman tidak valid</p>`)
+        break 
 }
