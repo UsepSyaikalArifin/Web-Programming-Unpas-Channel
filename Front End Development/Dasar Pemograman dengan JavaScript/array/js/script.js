0dd53898fe570a976eb85a840ffa9c310268802a
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-08 21:55:30 +0700

    Array

---
+++ Front End Development/Dasar Pemograman dengan JavaScript/array/js/script.js
@@ -0,0 +1,11 @@
+// 1. Contoh Array
+const hari = [`Senin`,`Selasa`,`Rabu`,`Kamis`,`Jum'at`,`Sabtu`,`Minggu`]
+
+for(i of hari){
+    document.writeln(`<p>${i}</p>`)
+}
+
+// 2.
+let binatang = [`Sapi`,`Kerbau`,`Sapi`,`Kambing`,`Unta`,`Ayam`]
+document.writeln(`<p>${binatang[0]}</p>`)
+document.writeln(`<p>${binatang.length}</p>`)
