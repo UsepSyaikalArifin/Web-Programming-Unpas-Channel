da876c08b79e7e69f816e009b2a517ea8b0efa24
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-08 20:48:46 +0700

    Variable Scope JavaScript

---
+++ Front End Development/Dasar Pemograman dengan JavaScript/scope-javascript/js/script.js
@@ -0,0 +1,19 @@
+// 1. Contoh - 1
+// let i
+// if(i % 2 === 0){
+//     let genap = true
+// }
+// if(genap){
+//     document.writeln(`<p>${genap}</p>`)
+// }
+
+// 2. Contoh - 2
+let a = 1
+function test(){
+    let b = 2
+    console.log(a)
+    console.log(b)
+}
+
+test()
+console.log(b)
