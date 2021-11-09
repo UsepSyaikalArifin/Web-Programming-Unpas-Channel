4a5f9432e42ef4f6f176782cf2483388261cc09a
UsepSyaikalArifin <useparifinsyaikal@gmail.com>
2021-11-09 19:08:02 +0700

    Tugas Manipulasi Array

--- Front End Development/Dasar Pemograman dengan JavaScript/program-pengelolaan-mobil-angkot/js/script.js
+++ Front End Development/Dasar Pemograman dengan JavaScript/program-pengelolaan-mobil-angkot/js/script.js
@@ -1,1 +1,21 @@
-let penumpang = []
+// Buat array
+let penumpang = ["Usep",undefined]
+function tambahPenumpang(namaPenumpang, penumpang){
+    if(penumpang.length === 0){
+        penumpang.push(namaPenumpang)
+        return penumpang
+    }else{
+        for(let i = 0; i < penumpang.length; i++){
+            if(penumpang[i] === undefined){
+                penumpang[i] = namaPenumpang
+                return penumpang
+            }else if(penumpang[i] === namaPenumpang){
+                console.log("Penumpang sudah terdaftar")
+                return penumpang
+            }else if(i === penumpang.length - 1){
+                penumpang.push(namaPenumpang)
+                return penumpang
+            }
+        }
+    }
+}
